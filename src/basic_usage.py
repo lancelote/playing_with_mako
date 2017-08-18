161e1e3a48e17f34722574cb667e15575b139134
Pavel Karateev <karateev.pavel@ya.ru>
2017-08-18 21:43:22 +0300

    Basic usage chapter in progress

---
+++ src/basic_usage.py
@@ -0,0 +1,31 @@
+from mako.template import Template
+from mako.runtime import Context
+from io import StringIO
+from os.path import join
+
+
+# Simple example #
+##################
+
+my_template0 = Template('hello world')
+# print(my_template0.render())
+
+# Passing arguments #
+#####################
+
+my_template1 = Template('hello, ${name}!')
+# print(my_template1.render(name='Pavel'))
+
+# More details #
+################
+
+my_template2 = Template('hello, ${name}!')
+buffer = StringIO()
+context = Context(buffer, name='Pavel')
+my_template2.render_context(context)
+# print(buffer.getvalue())
+
+# File-based template #
+#######################
+
+my_template3 = Template(filename=join('template', 'my_template1.tpl'))
